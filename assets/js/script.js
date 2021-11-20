b2ec79890a30b4f969a2d40417b7cfd2f58eba82
tkcannon <taylorkcannon@gmail.com>
2021-11-20 12:24:52 -0700

    initial js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,30 @@
 //-high score /check localstorage for exisitng scores
 //-current score
 
+var timer = 60;
+var highscore = getHighScore();
+var currentscore;
+
 //functions
 //-questions
 //-timer
 //-score
+
+function getHighScore() {
+   var score = parseInt(localStorage.getItem("highscore"));
+
+   if (!score) {
+       return "";
+   }
+
+   else {
+       return score;
+   }
+}
+
+function startQuiz() {
+    
+}
 
 //steps
 //-start quiz

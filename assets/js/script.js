69899efd8b4f33a5342ae9c4fa10174c3fe50d04
tkcannon <taylorkcannon@gmail.com>
2021-11-20 16:23:54 -0700

    added start quiz section, and started js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,189 @@
 //-current score
 
 var timer = 60;
-var highscore = getHighScore();
-var currentscore;
-
 //functions
 //-questions
 //-timer
 //-score
 
-function getHighScore() {
-   var score = parseInt(localStorage.getItem("highscore"));
+var quiz = {
+    highScore: getHighScore(),
+    question: getById("question"),
+    answers: [
+        getById("btn-1"),
+        getById("btn-2"),
+        getById("btn-3"),
+        getById("btn-4"),
+    ],
+    timer: getById("timer")
+};
 
-   if (!score) {
-       return "";
-   }
-
-   else {
-       return score;
-   }
+//to shorten the call
+function getById(id) {
+    var el = document.getElementById(id);
+    return el;
 }
 
-function startQuiz() {
-    
+function getHighScore() {
+    var score = localStorage.getItem("high-score");
+    var scoreBoard = getById("high-score");
+    if (!score) {
+        scoreBoard.textContent = "High Score:";
+        return "";
+    }
+
+    else {
+        scoreBoard.textContent = "High Score: " + score;
+        return score;
+    }
+}
+
+function startQuiz(event) {
+    if (event.target.matches("#start-quiz")) {
+        var startPageDiv = getById("start-page");
+        var quizDiv = getById("quiz");
+        toggleDisplay(startPageDiv);
+        toggleDisplay(quizDiv);
+
+        var interval = setInterval(() => {
+            if(timer > 0) {
+                countDown();
+            }
+            else{
+                toggleDisplay(quizDiv);
+                clearInterval(interval);
+            }
+        }, 1000);
+
+        
+    }
+}
+
+function toggleDisplay(element) {
+    if (element.style.display === "none") {
+        element.style.display = "flex";
+    }
+
+    else {
+        element.style.display = "none";
+    }
+}
+
+function getQuestion(num) {
+    console.log("getQuestion " + num + " called");
+    var questions = [
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+        {
+            text: "1",
+            a1: "yes",
+            a2: "no",
+            a3: "maybe",
+            a4: "so?",
+            answer: "maybe"
+        },
+    ];
+
+    return questions[num];
+}
+
+function countDown () {
+    quiz.timer.textContent = timer;
+    timer--;
 }
 
 //steps
@@ -39,3 +200,5 @@ function startQuiz() {
 //-prompt try again
 
 //first call
+
+document.getElementById("start-quiz").addEventListener("click", startQuiz);

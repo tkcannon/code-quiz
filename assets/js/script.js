9ed90a8025893b9a0d81d067569e8704c39bcf16
tkcannon <taylorkcannon@gmail.com>
2021-11-20 11:05:24 -0700

    js psuedo code

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//global vars
+//-timer
+//-high score /check localstorage for exisitng scores
+//-current score
+
+//functions
+//-questions
+//-timer
+//-score
+
+//steps
+//-start quiz
+//-first question
+//-right/wrong add-points/deduct-time
+//-next question
+//-if time === 0/ end
+//-if all questions answered/ end
+//-show/claim score form
+//-prompt try again
+
+//first call
